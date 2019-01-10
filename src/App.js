22b24bf3bec4439fe07eeb6d066cf1cc8ec3409b
Aleksandar <ace92.work@gmail.com>
2019-01-10 13:16:08 +0100

    use setState

--- src/App.js
+++ src/App.js
@@ -5,13 +5,33 @@ import Person from './Person/Person';
 
 
 class App extends Component {
+  state = {
+    persons: [
+      { name:"Maxi",age:20},
+      { name:"Manu",age:36},
+      { name:"Stef",age:41},
+
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("klik");
+    this.setState({
+      persons: [
+        { name:"Seb",age:20},
+        { name:"Manu",age:36},
+        { name:"Stef",age:41},
+      ]
+    });
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi</h1> 
-        <Person name="Max" age="26"/>
-        <Person name="Manu" age="36">My Hobbies.</Person>
-        <Person name="Stef" age="46"/>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies.</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
